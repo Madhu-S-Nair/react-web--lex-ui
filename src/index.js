20ed26a884da3ff268b5d427f56fbb0ad2612aae
Madhu-S-Nair <99575267+Madhu-S-Nair@users.noreply.github.com>
2024-06-08 16:14:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+Amplify.configure(awsconfig);
+
 root.render(
   <React.StrictMode>
     <App />
